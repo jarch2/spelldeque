e5e5744f737fb80958608c4e851564e87238bc16
Joe Arch <josephrarch@gmail.com>
2023-10-01 01:19:36 -0400

    yup

--- Scripts/TileManager.gd
+++ Scripts/TileManager.gd
@@ -1,23 +1,29 @@
 extends Node
 
-@onready var screensize = DisplayServer.window_get_size()
+@onready var screensize = get_viewport().size
 @onready var tile_scene = preload("res://Prefabs/tile.tscn")
 
 var numRows = 4
 var numColumns = 3
 var tiles = []
 
+var gapRatio = 1/4
+var midGap = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var scale = 0.5
-	var hspace = screensize.x / (numColumns * 2)
-	var vspace = screensize.y / numRows
+	
+	var vertscale = screensize.y / (numRows + gapRatio*(numRows + 1))
+	var horscale = (screensize.x - midGap) / (2*numColumns + gapRatio*(2*numColumns + 1))
+	
+	var scale = min(vertscale, horscale)
 	for i in range(0,numRows):
-		for j in range(0,2*numColumns):
+		for j in range(0,numColumns):
 			var tile = tile_scene.instantiate()
-			tile.resize(scale)
-			tile.position = Vector2(75 * scale + hspace * j, 75 + vspace * i)
+			tile.resize(scale/100)
+			tile.position = Vector2((gapRatio + 0.5) * scale + (scale + gapRatio * scale) * j, (gapRatio + 0.5) * scale + (scale + gapRatio * scale) * i)
 			add_child(tile)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
