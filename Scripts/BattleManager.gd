1e1b9601a3ba4b2809d45f7eca459fa4a48a5fe8
Joe Arch <josephrarch@gmail.com>
2023-10-02 19:54:30 -0400

    no

--- Scripts/BattleManager.gd
+++ Scripts/BattleManager.gd
@@ -1,16 +1,32 @@
 extends Node
 
+@onready var player = preload("res://Prefabs/Player.tscn")
+@onready var screensize = get_viewport().size
+
 enum playState {play, execute}
 var currentState
 
-var endTurnButton
+var player1
+var player2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	endTurnButton = get_node("Button")
-	add_child(endTurnButton)
 	currentState = playState.play
+	player1 = player.instantiate()
+	player2 = player.instantiate()
+	player1.position = Vector2(screensize.x - 100, screensize.y - 100)
+	player2.position = Vector2(screensize.x - 100, screensize.y - 200)
+	player1.turn_end.connect(on_end_turn)
+	add_child(player1)
+	add_child(player2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func on_end_turn():
+	print("yeah")
+	pass
+
+func _attempt_execute():
 	pass
